4899c17f2c0442c08718fd38932cce2dd5b0f146
Xiphereal <lozanojalex@gmail.com>
2024-03-23 11:39:30 +0100

    test: split test cases

--- FizzBuzz/Tests/Class1.cs
+++ FizzBuzz/Tests/Class1.cs
@@ -6,13 +6,23 @@ namespace FizzBuzz.Tests
     public class Class1
     {
         [Fact]
-        public void ASdfasdfasdf()
+        public void NotMultipleOf3or5_ReturnNumber()
+        {
+            FizzBuzz.Of(1).Should().Be("1");
+        }
+
+        [Fact]
+        public void MultipleOf3_ShouldReturnFizz()
         {
             FizzBuzz.Of(3).Should().Be("Fizz");
             FizzBuzz.Of(9).Should().Be("Fizz");
+        }
+
+        [Fact]
+        public void MultipleOf5_ShouldReturnBuzz()
+        {
             FizzBuzz.Of(5).Should().Be("Buzz");
             FizzBuzz.Of(20).Should().Be("Buzz");
-            FizzBuzz.Of(1).Should().Be("1");
         }
     }
 }
