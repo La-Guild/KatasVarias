9c3dacbb9ff199db88c3938b62fc0954b70fdaa3
Xiphereal <lozanojalex@gmail.com>
2024-03-23 11:37:38 +0100

    feat: return Buzz for any multiple of 5

--- FizzBuzz/Tests/Class1.cs
+++ FizzBuzz/Tests/Class1.cs
@@ -11,6 +11,7 @@ namespace FizzBuzz.Tests
             FizzBuzz.Of(3).Should().Be("Fizz");
             FizzBuzz.Of(9).Should().Be("Fizz");
             FizzBuzz.Of(5).Should().Be("Buzz");
+            FizzBuzz.Of(20).Should().Be("Buzz");
             FizzBuzz.Of(1).Should().Be("1");
         }
     }
