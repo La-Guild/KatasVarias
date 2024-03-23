6c1bcea8225a5cc2dfa61f43902dd58d2d00317a
Joser Gala Naranjo <jgalanar@gmail.com>
2024-03-23 12:19:56 +0100

    refactor: extract constant

--- FizzBuzz/Tests/Asdfassssdf.cs
+++ FizzBuzz/Tests/Asdfassssdf.cs
@@ -2,9 +2,14 @@
 {
     internal class Asdfassssdf
     {
-        internal static double Aasdfasdf(int[] ints)
+        private const int BasePrice = 8;
+
+        internal static double Aasdfasdf(int[] basket)
         {
-            return 8;
+            if (basket.Length == 2)
+                return BasePrice * 2 * 0.95;
+
+            return BasePrice;
         }
     }
 }
