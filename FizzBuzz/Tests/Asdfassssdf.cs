d1d2feeb1ecbc32908818f921b5bf9f97d55b644
Joser Gala Naranjo <jgalanar@gmail.com>
2024-04-27 12:37:30 +0200

    refactor: remove duplication on applying discount

--- FizzBuzz/Tests/Asdfassssdf.cs
+++ FizzBuzz/Tests/Asdfassssdf.cs
@@ -6,28 +6,23 @@ namespace FizzBuzz.Tests
     internal class Asdfassssdf
     {
         private const int BasePrice = 8;
-        private Dictionary<int, double> priceAfterDiscount = new()
+
+        private static Dictionary<int, double> priceAfterDiscount = new()
         {
-            {2, 0.95},
-            {3, 0.90},
-            {4, 0.80},
+            { 2, 0.95 },
+            { 3, 0.90 },
+            { 4, 0.80 },
         };
 
         internal static double Aasdfasdf(int[] basket)
         {
             var sdogmasfkdgm = BasePrice * basket.Length;
-            
+
             if (HasDuplicates(basket))
                 return sdogmasfkdgm;
 
-            if (basket.Length == 2)
-                return sdogmasfkdgm * 0.95;
-
-            if (basket.Length == 3)
-                return sdogmasfkdgm * 0.90;
-
-            if (basket.Length == 4)
-                return sdogmasfkdgm * 0.80;
+            if (basket.Length > 1)
+                return sdogmasfkdgm * priceAfterDiscount[basket.Length];
 
             return BasePrice;
         }
