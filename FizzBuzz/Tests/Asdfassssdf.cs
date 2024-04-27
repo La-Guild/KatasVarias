17a28db9c5c1f89869783c4190faab4c17b1ae62
Joser Gala Naranjo <jgalanar@gmail.com>
2024-04-27 12:56:46 +0200

    refactor: introduce method instead of dictionary

--- FizzBuzz/Tests/Asdfassssdf.cs
+++ FizzBuzz/Tests/Asdfassssdf.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FizzBuzz.Tests
@@ -7,15 +8,6 @@ namespace FizzBuzz.Tests
     {
         private const int BasePrice = 8;
 
-        private static Dictionary<int, double> priceAfterDiscount = new()
-        {
-            { 1, 1.00 },
-            { 2, 0.95 },
-            { 3, 0.90 },
-            { 4, 0.80 },
-            { 5, 0.75 },
-        };
-
         internal static double Aasdfasdf(int[] basket)
         {
             var sdogmasfkdgm = BasePrice * basket.Length;
@@ -23,7 +15,20 @@ namespace FizzBuzz.Tests
             if (HasDuplicates(basket))
                 return sdogmasfkdgm;
 
-            return sdogmasfkdgm * priceAfterDiscount[basket.Length];
+            return sdogmasfkdgm * PriceAfterDiscountForDistinctBooks(basket.Length);
+        }
+
+        private static double PriceAfterDiscountForDistinctBooks(int howMany)
+        {
+            return howMany switch
+            {
+                1 => 1.00,
+                2 => 0.95,
+                3 => 0.90,
+                4 => 0.80,
+                5 => 0.75,
+                _ => throw new ArgumentException("No hay más de cinco chaval.")
+            };
         }
 
         private static bool HasDuplicates(int[] basket)
