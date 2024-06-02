c2b5289c8cf982a05c31a6c9fed802ca2af65247
Joser Gala Naranjo <jgalanar@gmail.com>
2024-06-02 11:07:38 +0200

    refactor: introduce new class

--- DbCKata/DbcKata/NotNegative.cs
+++ DbCKata/DbcKata/NotNegative.cs
@@ -7,11 +7,27 @@ public readonly struct NotNegative
     NotNegative(int howMuch)
     {
         if (howMuch < 0)
-            throw new ArgumentException("Coins must be positive");
-        
+            throw new ArgumentException("Coins must be not negative");
+
         this.howMuch = howMuch;
     }
-    
+
     public static implicit operator NotNegative(int howMuch) => new NotNegative(howMuch);
     public static implicit operator int(NotNegative purse) => purse.howMuch;
+}
+
+public readonly struct Positive
+{
+    readonly int howMuch;
+
+    Positive(int howMuch)
+    {
+        if (howMuch <= 0)
+            throw new ArgumentException("Coins must be positive");
+
+        this.howMuch = howMuch;
+    }
+
+    public static implicit operator Positive(int howMuch) => new Positive(howMuch);
+    public static implicit operator int(Positive purse) => purse.howMuch;
 }
