8a1e65c5398d9a21f3a55e3194bbb0b01520edb5
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-06-02 10:37:16 +0200

    contract: cannot recruit if coins are not sufficient

--- DbCKata/DbcKata/UnitTest1.cs
+++ DbCKata/DbcKata/UnitTest1.cs
@@ -2,14 +2,21 @@ namespace DbcKata;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void Test1()
     {
         Assert.Pass();
     }
+}
+
+public class asfasfsafas
+{
+    public void Recruit()
+    {
+        if(Coins < RecruitmentPrice)
+            throw new InvalidOperationException("No hay suficiente oro");
+    }
+
+    public int Coins { get; set; }
+    public int RecruitmentPrice { get; set; }
 }
