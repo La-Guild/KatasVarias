22c33b0f182da14e7b071baf5555575867503ae3
Xiphereal <lozanojalex@gmail.com>
2024-08-05 19:16:21 +0200

    dasdjkfhasdf

---
+++ SmartFridget/SmartFridget/Fridge.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SmartFridget
+{
+    internal class Fridge
+    {
+        public static Fridge Empty => new Fridge();
+
+        internal string ASDfasfd()
+        {
+            return "";
+        }
+    }
+}
--- SmartFridget/SmartFridget/UnitTest1.cs
+++ SmartFridget/SmartFridget/UnitTest1.cs
@@ -1,15 +1,19 @@
+using FluentAssertions;
+
 namespace SmartFridget;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+    // Meter item
+    // Mostrar estado items no caducados: $item.name: n day(s) remaining 
+    // Expired Items displayed as "EXPIRED: $item.name"
+    // Expired Items antes que los que los demas.
+    // Abiertos degradan en 5h cuando se abre el frigo
+    // Cerrados degradan en 1h cuando se abre el frigo
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        Fridge.Empty.ASDfasfd().Should().BeEmpty();
     }
 }
