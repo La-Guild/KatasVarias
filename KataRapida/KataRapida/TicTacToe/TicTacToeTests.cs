4022c757cf1309bdb9f386c7384636ebeb761dfb
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-10-11 16:19:11 +0200

    feat: initialize all cells free

--- KataRapida/KataRapida/TicTacToe/TicTacToeTests.cs
+++ KataRapida/KataRapida/TicTacToe/TicTacToeTests.cs
@@ -1,10 +1,23 @@
 namespace KataRapida;
 
+// [] Qué hay en una casilla (Si X o O)
+// [] Si una casilla está ocupada
+// [] Si hay 3 del mismo símbolo alineadas game over
+// [] Después de colocar una X, va una O.
+
 public class TicTacToeTests
 {
     [Test]
-    public void Test1()
+    public void CellIsFreeByDefault()
     {
-        Assert.Pass();
+        Assert.AreEqual(new TicTacToe().SymbolAt((0, 0)), "");
+    }
+}
+
+public class TicTacToe
+{
+    public string SymbolAt((int, int) cell)
+    {
+        return "";
     }
 }
